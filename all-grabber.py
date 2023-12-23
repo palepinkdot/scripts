2697e5e5582ac7b03137439c12913fd77b15bacd
Max Graman <me@maxg.xyz>
2023-12-23 00:08:01 -0500

    Fixed some things

--- all-grabber.py
+++ all-grabber.py
@@ -1,8 +1,16 @@
+User
+import requests
 import os
-import urllib.request
-import threading
-from urllib.parse import urljoin, unquote
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import colorama
+from colorama import Fore
+import random
+
+# Initialize colorama for colored console output
+colorama.init()
 
 # URLs of the webpages you want to scrape
 urls = [
@@ -11,74 +19,79 @@ urls = [
     "https://archive.org/download/WiiRomSetByGhostwarePart3/"
 ]
 
-class LinkParser(HTMLParser):
-    """ Parser to find links in HTML content. """
-    def __init__(self, base_url):
-        super().__init__()
-        self.base_url = base_url
-        self.file_urls = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for attr in attrs:
-                if attr[0] == 'href' and attr[1].endswith('.wbfs'):
-                    full_url = urljoin(self.base_url, attr[1])
-                    self.file_urls.append(full_url)
-
-def download_file(url, progress, lock):
+def download_file(url, download_directory, color):
     """Download a file from a URL and save it in the specified directory."""
     try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
         file_name = unquote(url.split('/')[-1])
-        download_path = os.path.join(download_directory, file_name)
+        # Splitting the file name to extract game name and code
+        game_name_code = file_name.rsplit('.wbfs', 1)[0]  # Remove the '.wbfs' extension
+        game_name, game_code = game_name_code.rsplit(' [', 1)
+        game_code = game_code.replace(']', '')  # Remove the closing bracket
 
-        with urllib.request.urlopen(url) as response, open(download_path, 'wb') as out_file:
-            data = response.read()
-            out_file.write(data)
 
-        with lock:
-            progress[0] += 1
-            print_progress_bar(progress[0], len(file_urls))
+        # Creating a directory for the game if it doesn't exist
+        game_directory = os.path.join(download_directory, f"{game_name} [{game_code}]")
+        os.makedirs(game_directory, exist_ok=True)
 
-    except Exception as e:
-        with lock:
-            print(f"Error downloading {url}: {e}")
+        # Setting the download path
+        download_path = os.path.join(game_directory, f"{game_code}.wbfs")
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        print(color + f"Downloading file: {file_name}")
+        print(f"From URL: {url}")
+        print(f"Expected size: {total_size_in_bytes / 1024 / 1024:.2f} MB" + Fore.RESET)
 
-def print_progress_bar(current, total):
-    """Prints a simple text-based progress bar."""
-    percent = (current / total) * 100
-    bar = '#' * int(percent) + '-' * (100 - int(percent))
-    print(f"\r[{bar}] {current}/{total} files downloaded", end='')
+        block_size = 1024  # 1 Kibibyte
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+        with open(download_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print(color + "ERROR, something went wrong" + Fore.RESET)
+
+        print(color + f"Downloaded {file_name}\n" + Fore.RESET)
+
+    except requests.HTTPError as http_err:
+        print(color + f"HTTP error occurred: {http_err}" + Fore.RESET)
+    except Exception as err:
+        print(color + f"An error occurred: {err}" + Fore.RESET)
 
 # Create a directory for the downloaded files
-download_directory = "downloaded_files"
+download_directory = "wbfs"
 os.makedirs(download_directory, exist_ok=True)
 
-# Collect all file URLs
+# Define a list of colors for console output
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+
+# List to store file URLs
 file_urls = []
+
+# Extract file URLs from each page
 for url in urls:
-    with urllib.request.urlopen(url) as response:
-        html_content = response.read().decode('utf-8')
-        parser = LinkParser(url)
-        parser.feed(html_content)
-        file_urls.extend(parser.file_urls)
+    response = requests.get(url)
+    response.raise_for_status() 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.find_all('a')
 
-# Function to download files in batches of 5
-def download_batch(file_urls, progress, lock):
-    threads = []
-    for url in file_urls:
-        thread = threading.Thread(target=download_file, args=(url, progress, lock))
-        thread.start()
-        threads.append(thread)
+    for link in links:
+        href = link.get('href')
+        if href and href.endswith('.wbfs'):
+            file_url = url + href
+            file_urls.append(file_url)
 
-    for thread in threads:
-        thread.join()
+# Number of threads to use for downloading
+num_threads = 5
 
-# Initialize progress and lock for thread safety
-progress = [0]  # Using a list to allow modification in threads
-lock = threading.Lock()
+# Download files using multiple threads
+with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    for file_url in file_urls:
+        color = random.choice(colors)  # Randomly choose a color for each download
+        executor.submit(download_file, file_url, download_directory, color)
 
-# Download files in batches of 5
-for i in range(0, len(file_urls), 5):
-    download_batch(file_urls[i:i+5], progress, lock)
-
-print("\nAll files downloaded.")
+print(Fore.RESET + "All files downloaded.")
--- pick-grabber.py
+++ pick-grabber.py
@@ -1,8 +1,15 @@
+import requests
 import os
-import urllib.request
-from html.parser import HTMLParser
-from urllib.parse import urljoin, unquote
-import threading
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import colorama
+from colorama import Fore
+import random
+
+# Initialize colorama for colored console output
+colorama.init()
 
 # URLs of the webpages you want to scrape
 urls = [
@@ -11,55 +18,73 @@ urls = [
     "https://archive.org/download/WiiRomSetByGhostwarePart3/"
 ]
 
-class LinkParser(HTMLParser):
-    """ Parser to find links in HTML content. """
-    def __init__(self, base_url):
-        super().__init__()
-        self.base_url = base_url
-        self.file_urls = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for attr in attrs:
-                if attr[0] == 'href' and attr[1].endswith('.wbfs'):
-                    full_url = urljoin(self.base_url, attr[1])
-                    self.file_urls.append(full_url)
-
-def download_file(url, download_directory, progress, total_files):
+def download_file(url, download_directory, color):
     """Download a file from a URL and save it in the specified directory."""
     try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
         file_name = unquote(url.split('/')[-1])
-        download_path = os.path.join(download_directory, file_name)
+        # Splitting the file name to extract game name and code
+        game_name_code = file_name.rsplit('.wbfs', 1)[0]  # Remove the '.wbfs' extension
+        game_name, game_code = game_name_code.rsplit(' [', 1)
+        game_code = game_code.replace(']', '')  # Remove the closing bracket
 
-        with urllib.request.urlopen(url) as response, open(download_path, 'wb') as out_file:
-            data = response.read()
-            out_file.write(data)
 
-        progress[0] += 1
-        print_progress_bar(progress[0], total_files)
+        # Creating a directory for the game if it doesn't exist
+        game_directory = os.path.join(download_directory, f"{game_name} [{game_code}]")
+        os.makedirs(game_directory, exist_ok=True)
 
-    except Exception as e:
-        print(f"Error downloading {url}: {e}")
+        # Setting the download path
+        download_path = os.path.join(game_directory, f"{game_code}.wbfs")
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        print(color + f"Downloading file: {file_name}")
+        print(f"From URL: {url}")
+        print(f"Expected size: {total_size_in_bytes / 1024 / 1024:.2f} MB" + Fore.RESET)
 
-def print_progress_bar(current, total):
-    """Prints a simple text-based progress bar."""
-    bar_length = 50
-    progress_length = int((current / total) * bar_length)
-    bar = '#' * progress_length + '-' * (bar_length - progress_length)
-    print(f"\rProgress: [{bar}] {current}/{total}", end='')
+
+
+        block_size = 1024  # 1 Kibibyte
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+        with open(download_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print(color + "ERROR, something went wrong" + Fore.RESET)
+
+        print(color + f"Downloaded {file_name}\n" + Fore.RESET)
+
+    except requests.HTTPError as http_err:
+        print(color + f"HTTP error occurred: {http_err}" + Fore.RESET)
+    except Exception as err:
+        print(color + f"An error occurred: {err}" + Fore.RESET)
 
 # Create a directory for the downloaded files
-download_directory = "downloaded_files"
+download_directory = "wbfs"
 os.makedirs(download_directory, exist_ok=True)
 
-# Collect all file URLs
+# Define a list of colors for console output
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+
+# List to store file URLs
 file_urls = []
+
+# Extract file URLs from each page
 for url in urls:
-    with urllib.request.urlopen(url) as response:
-        html_content = response.read().decode('utf-8')
-        parser = LinkParser(url)
-        parser.feed(html_content)
-        file_urls.extend(parser.file_urls)
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.find_all('a')
+
+    for link in links:
+        href = link.get('href')
+        if href and href.endswith('.wbfs'):
+            file_url = url + href
+            file_urls.append(file_url)
 
 # Display available files with indices
 for i, file_url in enumerate(file_urls):
@@ -76,16 +101,13 @@ selected_indices = [int(index) for index in selected_indices if index.isdigit()]
 # Filter the URLs based on selected indices
 selected_file_urls = [file_urls[i] for i in selected_indices if i < len(file_urls)]
 
-# Download selected files with progress bar
-progress = [0]
-total_files = len(selected_file_urls)
-threads = []
-for file_url in selected_file_urls:
-    thread = threading.Thread(target=download_file, args=(file_url, download_directory, progress, total_files))
-    thread.start()
-    threads.append(thread)
+# Number of threads to use for downloading
+num_threads = 5
 
-for thread in threads:
-    thread.join()
+# Download files using multiple threads
+with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    for file_url in selected_file_urls:
+        color = random.choice(colors)  # Randomly choose a color for each download
+        executor.submit(download_file, file_url, download_directory, color)
 
-print("\nAll selected files downloaded.")
+print(Fore.RESET + "Selected files downloaded.")
